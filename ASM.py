1a84e21b80c00fe08300bf4ec30b05e45d23a948
sabyn2705 <trinhdihoc2705@gmail.com>
2025-10-06 23:37:48 +0700

    Nộp ASM Giai Đoạn 1

---
+++ ASM.py
@@ -0,0 +1,30 @@
+#ASM.01
+def nhap_danh_sach_nv():
+    pass
+
+def luu_vao_file():
+    pass
+
+def doc_tu_file():
+    pass
+
+def tim_nv_theo_ma():
+    pass
+
+def xoa_nv_theo_ma():
+    pass
+
+def capnhat_nv_theo_ma():
+    pass
+
+def tim_nv_theo_khoang_luong():
+    pass
+
+def sapxep_theo_hoten():
+    pass
+
+def sapxep_theo_thunhap():
+    pass
+
+def top5_thunhap():
+    pass
